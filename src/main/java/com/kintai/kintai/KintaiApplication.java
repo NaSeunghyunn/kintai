518e78e8a123d3316bead78460119e964aea19d3
SeungHyunNa <nasbong077@gmail.com>
2023-12-03 15:37:56 +0900

    create a kintai project

---
+++ src/main/java/com/kintai/kintai/KintaiApplication.java
@@ -0,0 +1,13 @@
+package com.kintai.kintai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KintaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KintaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kintai/kintai/KintaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kintai.kintai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KintaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
