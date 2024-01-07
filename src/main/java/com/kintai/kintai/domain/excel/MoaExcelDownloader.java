0f25d516ad1e0eb614cc17387242da2f01ef30af
SeungHyunNa <nasbong077@gmail.com>
2024-01-07 21:51:10 +0900

    test

---
+++ src/main/java/com/kintai/kintai/domain/excel/MoaExcelDownloader.java
@@ -0,0 +1,9 @@
+package com.kintai.kintai.domain.excel;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class MoaExcelDownloader {
+}
