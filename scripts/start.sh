0ad1b5753e12831935028791df9f3222e1edadd0
SeungHyunNa <nasbong077@gmail.com>
2024-01-07 22:39:03 +0900

    start.sh 修正

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,7 @@
 REPOSITORY="/home/ec2-user/app/git"
 PROJECT_NAME="kintai"
 JAR_FILE=$(ls -tr $REPOSITORY/ | grep SNAPSHOT.jar | tail -n 1)
+echo "> JAR FILE : $JAR_FILE"
 
 APP_LOG="$REPOSITORY/application.log"
 ERROR_LOG="$REPOSITORY/error.log"
@@ -16,7 +17,7 @@ cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
 # jar 実行
 echo ">>> $TIME_NOW > $JAR_FILE 実行" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $REPOSITORY/$JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo ">>> $TIME_NOW > 実行したプロセス： $CURRENT_PID" >> $DEPLOY_LOG
